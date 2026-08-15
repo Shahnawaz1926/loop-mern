de23e2422e0b077c7d0fae62e1f23792b5c26429
Shahnawaz1926 <mshahnawaza2002@gmail.com>
2026-08-15 18:15:04 +0530

    chore: configure CORS for production deployment

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const workspaceRoutes = require('./routes/workspaceRoutes');
 const feedbackRoutes = require('./routes/feedbackRoutes');
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*',
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
